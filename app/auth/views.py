1c90d6f89593b729226a958d170da6fdacacbdf7
Jason Balthis <jason.balthis@gmail.com>
2015-02-21 22:32:01 -0500

    server restructuring

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,23 @@
 """
     auth.views - app/auth/views.py
 """
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
 
-from flask import render_template
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
+
+
